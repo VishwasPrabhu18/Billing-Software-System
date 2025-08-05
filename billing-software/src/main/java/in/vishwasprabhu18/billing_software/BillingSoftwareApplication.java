db16273bbedac7b2cad47099a9a80fd7c0ab6494
VishwasPrabhu18 <prabhuvishwas18@gmail.com>
2025-08-05 17:48:56 +0530

    Spring boot initialized

---
+++ billing-software/src/main/java/in/vishwasprabhu18/billing_software/BillingSoftwareApplication.java
@@ -0,0 +1,13 @@
+package in.vishwasprabhu18.billing_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingSoftwareApplication.class, args);
+	}
+
+}
